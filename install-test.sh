aae9aa6bba8f89990cb78236b93b18fb85d32a22
Colin Hutchinson <chutchic@gmail.com>
2022-12-08 06:17:54 +0000

    chore(tests): reusing parent image tests means less repitition and a clean test file

--- install-test.sh
+++ install-test.sh
@@ -10,15 +10,19 @@ if [ -n "${DEBUG:-}" ]; then
 fi
 
 function test() {
-    /usr/local/openresty/bin/openresty -v
-    #ldd /usr/local/openresty/bin/openresty | grep -q '/usr/local/kong/lib/libssl.so*'
-    #ldd /usr/local/openresty/bin/openresty | grep -q '/usr/local/kong/lib/libcrypto.so*'
-    /usr/local/openresty/bin/openresty -V 2>&1 | grep pcre
-    /usr/local/openresty/bin/resty -e 'print(jit.version)' | grep -q 'LuaJIT[[:space:]][[:digit:]]\+.[[:digit:]]\+.[[:digit:]]\+-[[:digit:]]\{8\}'
+    echo '--- testing kong ---'
+    cp -R /tmp/build/* /
 
-    ls -l /usr/local/openresty/lualib/resty/websocket/*.lua
-    grep _VERSION /usr/local/openresty/lualib/resty/websocket/*.lua
-    luarocks --version
+    mv /tmp/build /tmp/buffer # Check we didn't link dependencies to `/tmp/build/...`
+    ls -l /etc/kong/kong.conf.default
+    ls -l /etc/kong/kong*.logrotate
+    ls -l /usr/local/kong/include/google/protobuf/*.proto
+    ls -l /usr/local/kong/include/openssl/*.h
+
+    kong version
+
+    mv /tmp/buffer /tmp/build
+    echo '--- tested kong ---'
 }
 
 test
